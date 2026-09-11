9d80fe328998b87b2b831094aa8f74557ac7eebc
Matthew <matthewseaber@outlook.com>
2026-09-11 17:42:36 +0100

    Started to set up the main page and added the project's colour scheme to 'globals.css'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,68 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { ArrowRight } from "lucide-react";
+
 export default function Home() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [lastUpdated] = useState(new Date());
+  const [lastUpdatedVisible, setLastUpdatedVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLastUpdatedVisible((prevVisible) => !prevVisible);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="matrix-dots flex items-center justify-center min-h-screen p-6 font-mono">
+        <h1 className="text-4xl">Fetching the latest data...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="">
-      <p>Realtime</p>
+    <div className="matrix-dots flex flex-col gap-4 min-h-screen p-12 font-mono">
+      <div className="flex flex-row items-center justify-between gap-4">
+        <h1 className="flex flex-row items-center gap-4 text-7xl">
+          <span>{process.env.NEXT_PUBLIC_START_PLACE_NAME}</span>
+          <ArrowRight className="shrink-0 size-12" />
+          <span>{process.env.NEXT_PUBLIC_END_PLACE_NAME}</span>
+        </h1>
+
+        <div className="text-theme-blue">
+          {!lastUpdatedVisible ? (
+            <p className="text-3xl">{currentTime.toLocaleTimeString()}</p>
+          ) : (
+            <div className="flex flex-col items-end">
+              <p className="text-lg">Last updated</p>
+              <p
+                className={`text-3xl ${lastUpdated.getTime() < currentTime.getTime() - 600000 ? "text-theme-red" : lastUpdated.getTime() < currentTime.getTime() - 300000 ? "text-theme-amber" : ""}`}
+              >
+                {lastUpdated.toLocaleTimeString(undefined, {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div></div>
     </div>
   );
 }
