d18044f8a9cfebdea7648f1e7c7e0ff2d68ec71e
Matthew <matthewseaber@outlook.com>
2026-09-13 10:46:53 +0100

    Shaped the response from the RTT API to easily compare multiple routes and display relevant data

--- lib/providers/train-leg-info.ts
+++ lib/providers/train-leg-info.ts
@@ -8,9 +8,106 @@ interface TrainLeg {
   toName: string;
 }
 
+interface RTTTimeData {
+  scheduleInternal?: string;
+  scheduleAdvertised?: string;
+
+  realtimeForecast?: string;
+  realtimeEstimate?: string;
+  realtimeActual?: string;
+
+  realtimeNoReport?: boolean;
+
+  realtimeInternalLateness?: number;
+  realtimeAdvertisedLateness?: number;
+
+  isCancelled?: boolean;
+  cancellationReasonCode?: string;
+}
+
+interface RTTService {
+  temporalData: {
+    arrival?: RTTTimeData;
+    departure?: RTTTimeData;
+    pass?: RTTTimeData;
+
+    scheduledCallType?: string | null;
+    realtimeCallType?: string | null;
+    displayAs?: string | null;
+    status?: string | null;
+
+    isInterpolated?: boolean;
+  };
+
+  locationMetadata: {
+    platform?: {
+      planned?: string;
+      forecast?: string;
+      actual?: string;
+    };
+
+    line?: {
+      planned?: string;
+      forecast?: string;
+      actual?: string;
+    };
+
+    path?: {
+      planned?: string;
+      forecast?: string;
+      actual?: string;
+    };
+
+    numberOfVehicles?: number;
+    allocationIndex?: number;
+    isRequestStop?: boolean;
+  };
+
+  scheduleMetadata: {
+    uniqueIdentity: string;
+    namespace: string;
+    identity: string;
+    departureDate: string;
+
+    operator: {
+      code: string;
+      name: string;
+    };
+
+    modeType: "TRAIN" | "SHIP" | "BUS" | "SCHEDULED_BUS" | "REPLACEMENT_BUS";
+    inPassengerService: boolean;
+  };
+}
+
+interface TrainData {
+  id: string;
+
+  operatorName: string;
+
+  arrival: {
+    scheduled: string;
+    estimated?: string;
+  };
+
+  departure: {
+    scheduled: string;
+    estimated?: string;
+  };
+
+  platform?: string;
+
+  status: "on time" | "delayed" | "cancelled";
+  delayMinutes: number;
+}
+
 export async function fetchTrainData(leg: TrainLeg, currentTime: Date) {
   try {
-    const params = new URLSearchParams({});
+    const params = new URLSearchParams({
+      code: `gb-nr:${leg.to}`,
+      filterFrom: `gb-nr:${leg.from}`,
+      timeFrom: new Date().toISOString(),
+      timeTo: currentTime.toISOString(),
+    });
 
     const response = await fetch(
       `https://data.rtt.io/rtt/location?${params.toString()}`,
@@ -27,7 +124,54 @@ export async function fetchTrainData(leg: TrainLeg, currentTime: Date) {
 
     const data = await response.json();
 
-    return NextResponse.json(data);
+    const trainData: TrainData[] = data.services.map((service: RTTService) => {
+      const scheduleData = service.scheduleMetadata;
+      const temporalData = service.temporalData;
+      const locationData = service.locationMetadata;
+
+      const delayMinutes =
+        temporalData.arrival?.realtimeAdvertisedLateness ??
+        temporalData.departure?.realtimeAdvertisedLateness ??
+        0;
+      const status: TrainData["status"] =
+        temporalData.arrival?.isCancelled || temporalData.departure?.isCancelled
+          ? "cancelled"
+          : delayMinutes > 1
+            ? "delayed"
+            : "on time";
+
+      return {
+        id: scheduleData.uniqueIdentity,
+
+        operatorName: scheduleData.operator.name,
+
+        arrival: {
+          scheduled: temporalData.arrival?.scheduleAdvertised ?? "",
+          estimated:
+            temporalData.arrival?.realtimeActual ??
+            temporalData.arrival?.realtimeForecast ??
+            "",
+        },
+
+        departure: {
+          scheduled: temporalData.departure?.scheduleAdvertised ?? "",
+          estimated:
+            temporalData.departure?.realtimeActual ??
+            temporalData.departure?.realtimeForecast ??
+            "",
+        },
+
+        platform:
+          locationData.platform?.actual ??
+          locationData.platform?.forecast ??
+          locationData.platform?.planned,
+
+        status,
+        delayMinutes,
+      };
+    });
+
+    return NextResponse.json(trainData, { status: 200 });
   } catch (error) {
     console.log("Error fetching train data:", error);
 
