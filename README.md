645fca71dd0f5c5607250c04429361b92ed6d975
Matthew <matthewseaber@outlook.com>
2026-09-14 23:09:56 +0100

    Started to implement Neon/Drizzle and Better Auth for authentication

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 🚆 Realtime
+
+## Live demo
+👉 [Realtime website](https://realtime-gamma-three.vercel.app/) (hosted on Vercel using Next.js)
---
+++ db/schema.ts
@@ -0,0 +1,61 @@
+import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
+
+import { randomUUID } from "crypto";
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey().default(randomUUID()),
+  email: text("email").notNull().unique(),
+  username: text("username").notNull().unique(),
+  image: text("image"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+});
+
+export const account = pgTable("account", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  issuer: text("issuer").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  accountId: text("account_id").notNull(),
+  password: text("password"),
+  providerId: text("provider_id").notNull(),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  idToken: text("id_token"),
+  scope: text("scope"),
+});
+
+export const session = pgTable("session", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  token: text("token").notNull().unique(),
+  ipAddress: text("ip_address"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  userAgent: text("user_agent"),
+});
+
+export const verification = pgTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
